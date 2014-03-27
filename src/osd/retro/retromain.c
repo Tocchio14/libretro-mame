d0b4ec72d2d430cd48ece81a8758d0b778484983
r-type <r-type@users.noreply.github.com>
2014-03-27 16:58:18 +0100

    Rewrite experimental parse cmdline
    handle usage " for space in filename.

--- src/osd/retro/retromain.c
+++ src/osd/retro/retromain.c
@@ -604,6 +604,61 @@ int executeGame(char* path) {
 //  main
 //============================================================
 
+void parse_cmdline( const char *argv ){
+
+	static char buffer[512*2];
+	strcpy(buffer,argv);
+
+	char *p,*p2,*start_of_word;
+	int c,c2;
+	enum states { DULL, IN_WORD, IN_STRING } state = DULL;
+
+	for (p = buffer; *p != '\0'; p++) {
+		c = (unsigned char) *p; /* convert to unsigned char for is* functions */
+		switch (state) {
+			case DULL: /* not in a word, not in a double quoted string */
+				if (isspace(c)) {
+					/* still not in a word, so ignore this char */
+				continue;
+			}
+			/* not a space -- if it's a double quote we go to IN_STRING, else to IN_WORD */
+			if (c == '"') {
+				state = IN_STRING;
+				start_of_word = p + 1; /* word starts at *next* char, not this one */
+				continue;
+			}
+			state = IN_WORD;
+			start_of_word = p; /* word starts here */
+			continue;
+
+		case IN_STRING:
+			/* we're in a double quoted string, so keep going until we hit a close " */
+			if (c == '"') {
+				/* word goes from start_of_word to p-1 */
+				//... do something with the word ...
+				for (c2=0,p2 = start_of_word; p2 <p; p2++,c2++)ARGUV[ARGUC][c2] = (unsigned char) *p2;
+				ARGUC++; 
+				
+				state = DULL; /* back to "not in word, not in string" state */
+			}
+			continue; /* either still IN_STRING or we handled the end above */
+
+		case IN_WORD:
+			/* we're in a word, so keep going until we get to a space */
+			if (isspace(c)) {
+				/* word goes from start_of_word to p-1 */
+				//... do something with the word ...
+				for (c2=0,p2 = start_of_word; p2 <p; p2++,c2++)ARGUV[ARGUC][c2] = (unsigned char) *p2;
+				ARGUC++; 
+				
+				state = DULL; /* back to "not in word, not in string" state */
+			}
+			continue; /* either still IN_WORD or we handled the end above */
+		}	
+	}
+	
+}
+
 #ifdef __cplusplus
 extern "C"
 #endif
@@ -618,7 +673,7 @@ int mmain(int argc, const char *argv)
 	strcpy(gameName,argv);
 
 	if(experimental_cmdline/*commandline_enabled*/){
-
+/*
 		strcpy(tempstring,gameName);
 	        pch = strtok (tempstring," ");
    
@@ -629,7 +684,8 @@ int mmain(int argc, const char *argv)
 	
 	   		pch = strtok (NULL, " ");
 	  	}
-	
+*/	  	
+		parse_cmdline(argv);
 	        write_log("executing from experimental cmdline... %s\n", gameName);
 		result = executeGame(ARGUV[ARGUC-1]);
 	}
